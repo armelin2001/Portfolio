777cd77480ad978e69840cd0380093976ca7a984
Matheus Armelin <55255174+armelin2001@users.noreply.github.com>
2020-01-23 15:51:54 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Portifolio.Models;
+
+namespace Portifolio.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+        public IActionResult Sobre() {
+            return View();
+        }
+
+        public IActionResult Contato() {
+            return View();
+        }
+        public IActionResult Portifolio() {
+            return View();
+        }
+        public IActionResult Skills() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
